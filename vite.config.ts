b8a9f3bddcbea1201fdec7132d66760b7866d913
fafa-a <fabienpuech1@gmail.com>
2022-05-10 15:32:18 +0200

    feat: del index.css

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import type { PluginOption } from "vite"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import UnoCss from "unocss/vite"
-import presetUno from "@unocss/preset-uno"
 import presetAttributify from "@unocss/preset-attributify"
 import presetWebFonts from "@unocss/preset-web-fonts"
+import presetUno from "@unocss/preset-uno"
 // https://vitejs.dev/config/
 const plugins: (PluginOption | PluginOption[])[] = [
   UnoCss({
